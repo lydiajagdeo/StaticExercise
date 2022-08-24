046d2b778eab52a51d40b3db17f4f877c11adfb7
“Lydia <lydiajagdeo@gmail.com>
2022-08-24 11:33:15 -0400

    StaticExercise

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelcius(68); 
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20);
+
+            Console.WriteLine($"Celcius: {celsius}");
+            Console.WriteLine($"Fahrenheit: {fahrenheit}");
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        //public TempConverter()
+        //{
+        //}
+
+        public static double FahrenheitToCelcius( double fahrenheit)
+        {
+            return (fahrenheit - 32) / 1.8;
+            
+        }
+
+        public static double CelciusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }
+}
+
